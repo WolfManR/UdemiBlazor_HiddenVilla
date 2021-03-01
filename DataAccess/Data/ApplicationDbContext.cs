6856c060e5a434030c1875c75606c688e9f86464
Tarxos <ftarxos@gmail.com>
2021-03-02 02:11:17 +0300

    Creating Database

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+    }
+}

6deb33984aec396c7e1874befdcdaa79b2fd5fb8
Tarxos <ftarxos@gmail.com>
2021-03-22 22:16:05 +0300

    Hotel Room db model

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -5,5 +5,7 @@ namespace DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        public DbSet<HotelRoom> HotelRooms { get; set; }
     }
 }

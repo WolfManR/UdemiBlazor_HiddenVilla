8a4426f076b118c80d6f1e1a42febc7a163d2a2a
Tarxos <ftarxos@gmail.com>
2021-05-28 01:11:56 +0300

    HotelRoomImage

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -7,5 +7,6 @@ namespace DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
 
         public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<HotelRoomImage> HotelRoomImages { get; set; }
     }
 }

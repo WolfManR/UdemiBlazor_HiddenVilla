84437e46e17bd9ba6d188b134d7979feb484eee2
Tarxos <ftarxos@gmail.com>
2021-05-29 00:24:18 +0300

    FileUpload service

---
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using HiddenVilla_Server.Service.IService;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HiddenVilla_Server.Service
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            try
+            {
+                FileInfo fileInfo = new(file.Name);
+                var fileName = Guid.NewGuid() + fileInfo.Extension;
+                var folderDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages");
+                var path = Path.Combine(folderDirectory, fileName);
+
+                var memoryStream = new MemoryStream();
+                await file.OpenReadStream().CopyToAsync(memoryStream);
+
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fs);
+                }
+
+                var fullPath = $"RoomImages/{fileName}";
+                return fullPath;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteFile(string filename)
+        {
+            try
+            {
+                var path = $"{_webHostEnvironment.WebRootPath}\\RoomImages\\{filename}";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+
+                return false;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}

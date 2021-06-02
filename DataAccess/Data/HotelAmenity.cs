763df2904e55758bc4ffc34d8da1c81a484cb6c5
Tarxos <ftarxos@gmail.com>
2021-06-03 01:41:04 +0300

    add Hotel Amenity db model, and update database

---
+++ DataAccess/Data/HotelAmenity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Data
+{
+    public class HotelAmenity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Timing { get; set; }
+        [Required]
+        public string IconStyle { get; set; }
+    }
+}

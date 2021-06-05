205cf21278e12864614dd7bf6ea54abaffc86a8d
Tarxos <ftarxos@gmail.com>
2021-06-06 02:30:39 +0300

    add anotations for amenity dto model

--- Models/HotelAmenityDTO.cs
+++ Models/HotelAmenityDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class HotelAmenityDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter amenity name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter amenity description")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter amenity timing")]
         public string Timing { get; set; }
+
+        [Required(ErrorMessage = "Please enter amenity icon from font awesome")]
         public string IconStyle { get; set; }
     }
 }

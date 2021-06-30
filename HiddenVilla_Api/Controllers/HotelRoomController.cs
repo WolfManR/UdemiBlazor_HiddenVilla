baf98d3661d159d099450cd7b9045c5ff4d31820
Tarxos <ftarxos@gmail.com>
2021-06-30 21:31:54 +0300

    get hotel rooms api

---
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Repository.IRepository;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelRoomController : ControllerBase
+    {
+        private readonly IHotelRoomRepository hotelRoomRepository;
+
+        public HotelRoomController(IHotelRoomRepository hotelRoomRepository)
+        {
+            this.hotelRoomRepository = hotelRoomRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allRooms = await hotelRoomRepository.Get().ConfigureAwait(false);
+            return Ok(allRooms);
+        }
+    }
+}

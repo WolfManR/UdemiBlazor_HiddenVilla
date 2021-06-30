9effd08ae5f50f36bb005aae15b17542b6b2a46c
Tarxos <ftarxos@gmail.com>
2021-06-30 22:13:47 +0300

    Error model and  Get Room by id behavior in web api

--- HiddenVilla_Api/Controllers/HotelRoomController.cs
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -3,6 +3,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using Models;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,24 @@ namespace HiddenVilla_Api.Controllers
         {
             var allRooms = await hotelRoomRepository.Get().ConfigureAwait(false);
             return Ok(allRooms);
+        }
+
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> Get(int? roomId)
+        {
+            if (roomId is null)
+            {
+                return BadRequest(new ErrorModel { ErrorMessage = "Invalid Room Id", StatusCode = StatusCodes.Status400BadRequest });
+            }
+
+            var roomDetails = await hotelRoomRepository.Get(roomId.Value);
+
+            if (roomDetails is null)
+            {
+                return BadRequest(new ErrorModel { ErrorMessage = "Invallid Room Id", StatusCode = StatusCodes.Status404NotFound });
+            }
+
+            return Ok(roomDetails);
         }
     }
 }

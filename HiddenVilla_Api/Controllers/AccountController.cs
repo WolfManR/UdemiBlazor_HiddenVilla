f5731e9cf6f68feef317dd5a33fa0092ae09b07b
Tarxos <ftarxos@gmail.com>
2021-07-01 00:10:54 +0300

    SignUp behavior in account controller on web api

---
+++ HiddenVilla_Api/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Common;
+
+using DataAccess.Data;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using Models;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignUp([FromBody] UserRequestDTO request)
+        {
+            if(request is null || !ModelState.IsValid) return BadRequest();
+
+            var user = new ApplicationUser
+            {
+                UserName = request.Email,
+                Email=request.Email,
+                Name = request.Name,
+                PhoneNumber = request.PhoneNo,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e=>e.Description);
+                return BadRequest(new RegistrationResponseDTO { Errors = errors });
+            }
+
+            var roleResult = await userManager.AddToRoleAsync(user, SD.Role_Customer);
+            if (!roleResult.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponseDTO { Errors = errors });
+            }
+
+            return StatusCode(201);
+        }
+    }
+}

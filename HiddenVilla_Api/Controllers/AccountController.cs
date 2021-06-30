2f52347103c3422157aef5f649f9db6c033b4990
Tarxos <ftarxos@gmail.com>
2021-07-01 01:54:37 +0300

    SignIn behavior

--- HiddenVilla_Api/Controllers/AccountController.cs
+++ HiddenVilla_Api/Controllers/AccountController.cs
@@ -2,13 +2,22 @@
 
 using DataAccess.Data;
 
+using HiddenVilla_Api.Helper;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 using Models;
 
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HiddenVilla_Api.Controllers
@@ -21,12 +30,17 @@ namespace HiddenVilla_Api.Controllers
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly UserManager<ApplicationUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly APISettings apiSettings;
 
-        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AccountController(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IOptions<APISettings> options)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
             this.roleManager = roleManager;
+            apiSettings = options.Value;
         }
 
         [HttpPost]
@@ -60,6 +74,76 @@ namespace HiddenVilla_Api.Controllers
             }
 
             return StatusCode(201);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignIn([FromBody] AuthenticationDTO request)
+        {
+            var result = await signInManager.PasswordSignInAsync(request.UserName, request.Password, false, false);
+            if (result.Succeeded)
+            {
+                var user = await userManager.FindByNameAsync(request.UserName);
+                if (user is null)
+                {
+                    return Unauthorized(new AuthenticationResponseDTO
+                    {
+                        ErrorMessage = "Invalid Authentication"
+                    });
+                }
+
+                var signInCredentials = GetSigningCredentials();
+                var claims = await GetClaims(user);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: apiSettings.ValidIssuer,
+                    audience: apiSettings.ValidAudience,
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(30),
+                    signingCredentials: signInCredentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new AuthenticationResponseDTO
+                {
+                    IsAuthSuccessful = true,
+                    Token = token,
+                    UserDTO = new()
+                    {
+                        Id = user.Id,
+                        Name = user.Name,
+                        Email = user.Email,
+                        PhoneNo = user.PhoneNumber
+                    }
+                });
+            }
+
+            return Unauthorized(new AuthenticationResponseDTO { ErrorMessage = "Invalid Authentication" });
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(apiSettings.SecretKey));
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.Name, user.Email),
+                new(ClaimTypes.Email, user.Email),
+                new("Id", user.Id)
+            };
+
+            var roles = await userManager.GetRolesAsync(await userManager.FindByEmailAsync(user.Email));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new(ClaimTypes.Role, role));
+            }
+
+            return claims;
         }
     }
 }
---
+++ Models/AuthenticationResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class AuthenticationResponseDTO
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+        public UserDTO UserDTO { get; set; }
+    }
+}

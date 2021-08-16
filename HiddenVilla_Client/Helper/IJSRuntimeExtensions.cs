5022d4d20a8e71841a1dce37ae7b44e3f80eb4c4
Tarxos <ftarxos@gmail.com>
2021-08-17 01:57:03 +0300

    basic setup for client

---
+++ HiddenVilla_Client/Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+
+using Microsoft.JSInterop;
+
+namespace HiddenVilla_Client.Helper
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime self, string message)
+        {
+            await self.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime self, string message)
+        {
+            await self.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        public static async ValueTask SwalSuccess(this IJSRuntime self, string message, string title)
+        {
+            await self.InvokeVoidAsync("ShowSwal", "success", title, message);
+        }
+
+        public static async ValueTask SwalError(this IJSRuntime self, string message, string title)
+        {
+            await self.InvokeVoidAsync("ShowSwal", "error", title, message);
+        }
+    }
+}

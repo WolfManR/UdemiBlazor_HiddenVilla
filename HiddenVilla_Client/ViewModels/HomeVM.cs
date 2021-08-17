e45524da1b082de65e31376e49b2def3f7779d97
Tarxos <ftarxos@gmail.com>
2021-08-18 02:19:59 +0300

    add view model for Home Page and inject it to it

---
+++ HiddenVilla_Client/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HiddenVilla_Client.ViewModels
+{
+    public class HomeVM
+    {
+        public DateTime StartData { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public int NoOfNights { get; set; } = 1;
+    }
+}

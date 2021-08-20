306fa1e9aee4ea0acaf1df716e938a3197d91fed
Tarxos <ftarxos@gmail.com>
2021-08-21 02:10:49 +0300

    update database and separate work with ApplicationUser from IdentityUser

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
 
         public DbSet<HotelRoom> HotelRooms { get; set; }
         public DbSet<HotelRoomImage> HotelRoomImages { get; set; }
         public DbSet<HotelAmenity> HotelAmenities { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }

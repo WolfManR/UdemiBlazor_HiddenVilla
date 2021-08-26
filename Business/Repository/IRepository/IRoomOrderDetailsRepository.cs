13421ca597d07056b8d7ad7d11fd179a2afc3496
Tarxos <ftarxos@gmail.com>
2021-08-27 02:42:02 +0300

    prototype of room order details repository and create behavior

---
+++ Business/Repository/IRepository/IRoomOrderDetailsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+	public interface IRoomOrderDetailsRepository
+	{
+		Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details);
+		Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id);
+        Task<RoomOrderDetailsDTO> Get(int id);
+        Task<IEnumerable<RoomOrderDetailsDTO>> Get();
+        Task<bool> UpdateOrderStatus(int roomOrderId, string status);
+        Task<bool> IsRoomBooked(int roomId, DateTime checkInDate, DateTime checkOutDate);
+	}
+}
---
+++ Business/Repository/RoomOrderDetailsRepository.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+
+using Business.Repository.IRepository;
+
+using Common;
+
+using DataAccess.Data;
+
+using Models;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+	public class RoomOrderDetailsRepository : IRoomOrderDetailsRepository
+	{
+		private readonly ApplicationDbContext context;
+		private readonly IMapper mapper;
+
+		public RoomOrderDetailsRepository(ApplicationDbContext context, IMapper mapper)
+		{
+			this.context = context;
+			this.mapper = mapper;
+		}
+
+		public async Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details)
+		{
+			try
+			{
+				details.CheckInDate = details.CheckInDate.Date;
+				details.CheckOutDate = details.CheckOutDate.Date;
+				var roomOrder = mapper.Map<RoomOrderDetails>(details);
+				roomOrder.Status = SD.Status_Pending;
+				var result = await context.RoomOrderDetails.AddAsync(roomOrder);
+				await context.SaveChangesAsync();
+				return mapper.Map<RoomOrderDetailsDTO>(result);
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+		public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<RoomOrderDetailsDTO> Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<RoomOrderDetailsDTO>> Get()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateOrderStatus(int roomOrderId, string status)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> IsRoomBooked(int roomId, DateTime checkInDate, DateTime checkOutDate)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

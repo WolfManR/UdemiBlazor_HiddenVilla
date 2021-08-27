210839c635c8f9025977a0bf1e95d27d6485a350
Tarxos <ftarxos@gmail.com>
2021-08-28 02:21:08 +0300

    handle formating date times with extension, and initialize view model for room details

---
+++ HiddenVilla_Client/Helper/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToFormatedString(this DateTime self) => self.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+    }
+}

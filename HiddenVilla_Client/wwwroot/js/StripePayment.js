b4091b36f20829f11fca2adc26caec7e3b7e67d5
Tarxos <ftarxos@gmail.com>
2021-09-09 01:10:31 +0300

    inject stripe payment

---
+++ HiddenVilla_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51JWpRMH8V331WN56jBnOtHFt9sMWTUYr06yhA8dI4UXEKefJaWoRcugRPbUMD7I7fAlf3off1w7SmsKLFOarTvw400IWPb5Lqr');
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+}

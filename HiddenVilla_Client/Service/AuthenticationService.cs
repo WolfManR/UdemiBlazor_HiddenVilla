7bb5f6df77ba5acd567b06f6b42476b38d0235cd
Tarxos <ftarxos@gmail.com>
2021-09-10 00:46:32 +0300

    prototype login

---
+++ HiddenVilla_Client/Service/AuthenticationService.cs
@@ -0,0 +1,63 @@
+﻿using Blazored.LocalStorage;
+
+using HiddenVilla_Client.Service.IService;
+
+using Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using System.Net.Http.Headers;
+
+namespace HiddenVilla_Client.Service
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient client;
+        private readonly ILocalStorageService localStorage;
+
+        public AuthenticationService(HttpClient client, ILocalStorageService localStorage)
+        {
+            this.client = client;
+            this.localStorage = localStorage;
+        }
+
+        public Task<RegistrationResponseDTO> RegisterUser(UserRequestDTO userForRegistration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AuthenticationResponseDTO> Login(AuthenticationDTO userForAuthentication)
+        {
+            var content = JsonSerializer.Serialize(userForAuthentication);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("api/account/signin", bodyContent);
+
+            var contentTemp = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<AuthenticationResponseDTO>(contentTemp);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await localStorage.SetItemAsync(SD.Local_Token, result.Token);
+                await localStorage.SetItemAsync(SD.Local_UserDetails, result.UserDTO);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+
+                return new AuthenticationResponseDTO { IsAuthSuccessful = true };
+            }
+
+            return result;
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HiddenVilla_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDTO> RegisterUser(UserRequestDTO userForRegistration);
+        Task<AuthenticationResponseDTO> Login(AuthenticationDTO userForAuthentication);
+        Task Logout();
+    }
+}

73ecccb141483be4dea532b362343aeec072763b
Tarxos <ftarxos@gmail.com>
2021-09-10 01:42:29 +0300

    variant of razor component with code behind file

---
+++ HiddenVilla_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,45 @@
+﻿using HiddenVilla_Client.Service.IService;
+
+using Microsoft.AspNetCore.Components;
+
+using Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private readonly UserRequestDTO UserForRegistration = new UserRequestDTO();
+        public bool IsProcessing { get; set; }
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+
+            var result = await authenticationService.RegisterUser(UserForRegistration);
+            if (result.IsRegistrationSuccessful)
+            {
+                IsProcessing = false;
+                navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+        }
+    }
+}

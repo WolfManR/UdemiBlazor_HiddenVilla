087ceb935cea57cb3b7a3999bbed9d330dfa4ac9
Tarxos <ftarxos@gmail.com>
2021-09-12 00:51:34 +0300

    fixing get user info on sending payment

--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -3,65 +3,60 @@
 using Models;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Reflection;
-using System.Text.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HiddenVilla_Client.Service
 {
-    public class RoomOrderDetailsService : IRoomOrderDetailsService
-    {
-        private readonly HttpClient client;
+	public class RoomOrderDetailsService : IRoomOrderDetailsService
+	{
+		private readonly HttpClient client;
 
-        public RoomOrderDetailsService(HttpClient client)
-        {
-            this.client = client;
-        }
+		public RoomOrderDetailsService(HttpClient client)
+		{
+			this.client = client;
+		}
 
-        public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
-        {
-            details.UserId = "dummy user";
+		public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
+		{
+			var content = JsonSerializer.Serialize(details);
+			var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+			var response = await client.PostAsync("api/roomorder/create", bodyContent);
 
-            var content = JsonSerializer.Serialize(details);
-            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("api/roomorder/create", bodyContent);
+			if (response.IsSuccessStatusCode)
+			{
+				var contentTemp = await response.Content.ReadAsStringAsync();
+				var result = JsonSerializer.Deserialize<RoomOrderDetailsDTO>(contentTemp);
+				return result;
+			}
+			else
+			{
+				var contentTemp = await response.Content.ReadAsStringAsync();
+				var errorModel = JsonSerializer.Deserialize<ErrorModel>(contentTemp);
+				throw new Exception(errorModel.ErrorMessage);
+			}
+		}
 
-            if (response.IsSuccessStatusCode)
-            {
-                var contentTemp = await response.Content.ReadAsStringAsync();
-                var result = JsonSerializer.Deserialize<RoomOrderDetailsDTO>(contentTemp);
-                return result;
-            }
-            else
-            {
-                var contentTemp = await response.Content.ReadAsStringAsync();
-                var errorModel = JsonSerializer.Deserialize<ErrorModel>(contentTemp);
-                throw new Exception(errorModel.ErrorMessage);
-            }
-        }
+		public async Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
+		{
+			var content = JsonSerializer.Serialize(details);
+			var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+			var response = await client.PostAsync("api/roomorder/paymentsuccessfull", bodyContent);
 
-        public async Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
-        {
-            var content = JsonSerializer.Serialize(details);
-            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("api/roomorder/paymentsuccessfull", bodyContent);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var contentTemp = await response.Content.ReadAsStringAsync();
-                var result = JsonSerializer.Deserialize<RoomOrderDetailsDTO>(contentTemp);
-                return result;
-            }
-            else
-            {
-                var contentTemp = await response.Content.ReadAsStringAsync();
-                var errorModel = JsonSerializer.Deserialize<ErrorModel>(contentTemp);
-                throw new Exception(errorModel.ErrorMessage);
-            }
-        }
-    }
+			if (response.IsSuccessStatusCode)
+			{
+				var contentTemp = await response.Content.ReadAsStringAsync();
+				var result = JsonSerializer.Deserialize<RoomOrderDetailsDTO>(contentTemp);
+				return result;
+			}
+			else
+			{
+				var contentTemp = await response.Content.ReadAsStringAsync();
+				var errorModel = JsonSerializer.Deserialize<ErrorModel>(contentTemp);
+				throw new Exception(errorModel.ErrorMessage);
+			}
+		}
+	}
 }

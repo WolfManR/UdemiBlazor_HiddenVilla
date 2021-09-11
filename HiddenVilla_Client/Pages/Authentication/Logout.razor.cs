e9569e5a422bcfa64210d5e0dea316624a43f412
Tarxos <ftarxos@gmail.com>
2021-09-12 00:31:47 +0300

    logout component

---
+++ HiddenVilla_Client/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,23 @@
+﻿using HiddenVilla_Client.Service.IService;
+
+using Microsoft.AspNetCore.Components;
+
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await AuthenticationService.Logout();
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}

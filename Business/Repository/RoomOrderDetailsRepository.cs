447f911d6983d83c707febd0f8b1134dee5ef8c3
Tarxos <ftarxos@gmail.com>
2021-09-13 10:16:44 +0300

    prototype room order update status

--- Business/Repository/RoomOrderDetailsRepository.cs
+++ Business/Repository/RoomOrderDetailsRepository.cs
@@ -12,94 +12,106 @@ using Models;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Business.Repository
 {
-	public class RoomOrderDetailsRepository : IRoomOrderDetailsRepository
-	{
-		private readonly ApplicationDbContext context;
-		private readonly IMapper mapper;
+    public class RoomOrderDetailsRepository : IRoomOrderDetailsRepository
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
 
-		public RoomOrderDetailsRepository(ApplicationDbContext context, IMapper mapper)
-		{
-			this.context = context;
-			this.mapper = mapper;
-		}
+        public RoomOrderDetailsRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
 
-		public async Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details)
-		{
-			try
-			{
-				details.CheckInDate = details.CheckInDate.Date;
-				details.CheckOutDate = details.CheckOutDate.Date;
-				var roomOrder = mapper.Map<RoomOrderDetails>(details);
-				roomOrder.Status = SD.Status_Pending;
-				var result = await context.RoomOrderDetails.AddAsync(roomOrder);
-				await context.SaveChangesAsync();
-				return mapper.Map<RoomOrderDetailsDTO>(result);
-			}
-			catch (Exception)
-			{
-				return null;
-			}
-		}
-
-		public async Task<RoomOrderDetailsDTO> Get(int id)
-		{
-			try
-			{
-				var data = await context.RoomOrderDetails
-					.Include(u => u.HotelRoom).ThenInclude(r=>r.HotelRoomImages)
-					.FirstOrDefaultAsync(o=>o.Id == id);
-				if (data is null) return null;
-				var roomOrder = mapper.Map<RoomOrderDetailsDTO>(data);
-				roomOrder.HotelRoomDTO.TotalDays = roomOrder.CheckOutDate.Subtract(roomOrder.CheckInDate).Days;
-
-				return roomOrder;
-			}
-			catch (Exception)
-			{
-				return null;
-			}
-		}
-
-		public async Task<IEnumerable<RoomOrderDetailsDTO>> Get()
-		{
+        public async Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details)
+        {
             try
             {
-				var data = await context.RoomOrderDetails.Include(u => u.HotelRoom).ToListAsync();
-				var roomOrders = mapper.Map<IEnumerable<RoomOrderDetailsDTO>>(data);
-				return roomOrders;
+                details.CheckInDate = details.CheckInDate.Date;
+                details.CheckOutDate = details.CheckOutDate.Date;
+                var roomOrder = mapper.Map<RoomOrderDetails>(details);
+                roomOrder.Status = SD.Status_Pending;
+                var result = await context.RoomOrderDetails.AddAsync(roomOrder);
+                await context.SaveChangesAsync();
+                return mapper.Map<RoomOrderDetailsDTO>(result);
             }
             catch (Exception)
             {
-				return null;
+                return null;
             }
-		}
+        }
 
-		public async Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id)
-		{
-			var data = await context.RoomOrderDetails.FindAsync(id);
-			if(data is null) return null;
-			if (!data.IsPaymentSuccessful)
-			{
-				data.IsPaymentSuccessful = true;
-				data.Status = SD.Status_Booked;
-				var markPaymentSuccessfull = context.RoomOrderDetails.Update(data);
-				await context.SaveChangesAsync();
-				return mapper.Map<RoomOrderDetailsDTO>(markPaymentSuccessfull.Entity);
-			}
+        public async Task<RoomOrderDetailsDTO> Get(int id)
+        {
+            try
+            {
+                var data = await context.RoomOrderDetails
+                    .Include(u => u.HotelRoom).ThenInclude(r => r.HotelRoomImages)
+                    .FirstOrDefaultAsync(o => o.Id == id);
+                if (data is null) return null;
+                var roomOrder = mapper.Map<RoomOrderDetailsDTO>(data);
+                roomOrder.HotelRoomDTO.TotalDays = roomOrder.CheckOutDate.Subtract(roomOrder.CheckInDate).Days;
 
-			return new RoomOrderDetailsDTO();
-		}
+                return roomOrder;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
 
-		public Task<bool> UpdateOrderStatus(int roomOrderId, string status)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<IEnumerable<RoomOrderDetailsDTO>> Get()
+        {
+            try
+            {
+                var data = await context.RoomOrderDetails.Include(u => u.HotelRoom).ToListAsync();
+                var roomOrders = mapper.Map<IEnumerable<RoomOrderDetailsDTO>>(data);
+                return roomOrders;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
 
-		
-	}
+        public async Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int id)
+        {
+            var data = await context.RoomOrderDetails.FindAsync(id);
+            if (data is null) return null;
+            if (!data.IsPaymentSuccessful)
+            {
+                data.IsPaymentSuccessful = true;
+                data.Status = SD.Status_Booked;
+                var markPaymentSuccessfull = context.RoomOrderDetails.Update(data);
+                await context.SaveChangesAsync();
+                return mapper.Map<RoomOrderDetailsDTO>(markPaymentSuccessfull.Entity);
+            }
+
+            return new RoomOrderDetailsDTO();
+        }
+
+        public async Task<bool> UpdateOrderStatus(int roomOrderId, string status)
+        {
+            try
+            {
+                var roomOrder = await context.RoomOrderDetails.FirstOrDefaultAsync(u => u.Id == roomOrderId);
+                if (roomOrder is null) return false;
+
+                roomOrder.Status = status;
+                if (roomOrder.Status == SD.Status_CheckIn) roomOrder.ActualCheckInDate = DateTime.Now;
+                if (roomOrder.Status == SD.Status_CheckOut_Completed) roomOrder.ActualCheckOutDate = DateTime.Now;
+
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
 }
